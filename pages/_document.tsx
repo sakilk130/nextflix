3c3cde39c8892ef7a916a7d3958ebc9ce16ae635
sakilk130 <sakilk130@gmail.com>
2022-07-30 13:57:51 +0600

    feat: :sparkles: create banner component

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import { Html, Main, NextScript, Head } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
